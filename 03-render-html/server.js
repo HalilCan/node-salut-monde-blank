6eb43977b60ecf8a0e90572a74f06d76ef377ae0
HalilCan <halilcanmemoglu@gmail.com>
2017-07-26 21:46:38 +0300

    Simple rendering done

--- 03-render-html/server.js
+++ 03-render-html/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs =  require('fs');
 
 function onRequest(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {'Content-Type': 'text/HTML'});
   fs.readFile('./index.html', null, function(error, data) {
     if (error) {
       response.writeHead(404);
